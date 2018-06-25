562a5ec2099f18cb12d792900a13c1f0174acbd4
lindsey.tonn <lindsey.tonn@gmail.com>
2018-06-25 01:05:10 -0300

    Adicionado configuração de loadbalancer e configuração docker file.

---
+++ elb.tf
@@ -0,0 +1,75 @@
+# Fetch AZs in the current region
+data "aws_availability_zones" "available" {}
+
+# ALB Security group
+# This is the group you need to edit if you want to restrict access to your application
+resource "aws_security_group" "lb" {
+  name        = "tf-ecs-alb"
+  description = "controls access to the ALB"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Traffic to the ECS Cluster should only come from the ALB
+resource "aws_security_group" "ecs_tasks" {
+  name        = "tf-ecs-tasks"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = "${var.app_port}"
+    to_port         = "${var.app_port}"
+    security_groups = ["${aws_security_group.lb.id}"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_alb" "main" {
+  name            = "tf-ecs-chat"
+  subnets         = ["${aws_subnet.public.*.id}", "${aws_subnet.private.*.id}"]
+  security_groups = ["${aws_security_group.lb.id}"]
+}
+
+resource "aws_alb_target_group" "app" {
+  name        = "tf-ecs-chat"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "${aws_vpc.main.id}"
+  target_type = "ip"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Redirect all traffic from the ALB to the target group
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = "${aws_alb.main.id}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.app.id}"
+    type             = "forward"
+  }
+}
